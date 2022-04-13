77ed0abd11aed3aa9a845f3b1785f3b6e9506e41
nishu-murmu <nishumurmu017@gmail.com>
2022-04-14 00:54:54 +0530

    (fix): adding and updating new files

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -1,11 +1,26 @@
-import {Box, Stack} from '@chakra-ui/react'
+import { Box, Image, Heading } from "@chakra-ui/react";
 
 //files
 
 const About = () => {
-	return (
-    <Box>This is a blog.</Box>
-  )
-}
+  return (
+    <Box>
+      <Heading as="h1" size="xl">
+        About
+      </Heading>
+      <Image
+        className="profile-image"
+        align="center"
+        src={require("../../static/nishu.jpg")}
+        borderRadius="full"
+        boxSize="100px"
+        alt="Nishu Murmu"
+      ></Image>
+      <Heading as="h4" size="md" className="profile-heading">
+        <strong>Nishu Murmu</strong>
+      </Heading>
+    </Box>
+  );
+};
 
-export default About
+export default About;
--- src/components/sections/works.js
+++ src/components/sections/works.js
@@ -1,58 +1,15 @@
-import React, { Fragment } from "react";
-
-const List = [
-	{
-		id: 1,
-		href: "https://github.com/nishu-murmu/psychedelic-waves",
-		src: require("../../static/psychedelic.png"),
-		name: "psychedelic-waves",
-		className: "section_content_one",
-		info: "Psychedelic waves made by perlin noise and GLSL.",
-	},
-	{
-		id: 2,
-		href: "https://github.com/nishu-murmu/Eva-Unit-01",
-		src: require("../../static/eva.png"),
-		name: "eva",
-		className: "section_content_two",
-		info: "Eva robot smooth scroll info display.",
-	},
-	{
-		id: 3,
-		href: "https://github.com/nishu-murmu/psychedelic-waves",
-		src: require("../../static/psychedelic.png"),
-		name: "psychedelic-waves",
-		className: "section_content_three",
-		info: "Psychedelic waves made by perlin noise and GLSL.",
-	},
-	{
-		id: 4,
-		href: "https://github.com/nishu-murmu/Eva-Unit-01",
-		src: require("../../static/eva.png"),
-		name: "eva",
-		className: "section_content_four",
-		info: "Eva robot smooth scroll info display.",
-	}
-];
+import { Box, Heading } from "@chakra-ui/react";
+import WorksItem from "./worksItem";
 
 const Works = () => {
-	const list = List.map((index) => {
-		return (
-			// 1
-			<div key={index.id} className="item">
-				<a href={index.href}>
-					<img
-						src={index.src}
-						alt={index.name}
-						// height="180"
-						// width="280"
-					/>
-					<p>{index.info}</p>
-				</a>
-			</div>
-		);
-	});
-	return <Fragment>{list}</Fragment>;
+  return (
+    <Box>
+      <Heading>Works</Heading>
+      <Box>
+        <WorksItem />
+      </Box>
+    </Box>
+  );
 };
 
 export default Works;
